bbe6a36dae54294f905cbab91ed87ee000457892
roman kostyrin <enemy-iubip@mail.ru>
2021-08-11 11:23:18 +0300

    feat: finish noMoreTurns,  isDraw and getWinner modules

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,5 +1,10 @@
 class TicTacToe {
   constructor() {
+    // this.field = [
+    //   ['x', null, null],
+    //   [null, null, null],
+    //   [null, null, 'x'],
+    // ]
     this.field = [
       [null, null, null],
       [null, null, null],
@@ -10,10 +15,6 @@ class TicTacToe {
     this.winner = null
     this.isDrawResult = false
     this.isNoMoreTurns = false
-  }
-
-  getCurrentPlayerSymbol() {
-    return this.currentSymbol
   }
 
   nextTurn(rowIndex, columnIndex) {
@@ -31,9 +32,52 @@ class TicTacToe {
       })
     })
     if (score === 9) {
+      this.isNoMoreTurns = true
+    }
+
+    this.field.forEach((el, index) => {
+      if (
+        this.field[index][0] === this.field[index][1] &&
+        this.field[index][1] === this.field[index][2] &&
+        this.field[index][0] != null
+      ) {
+        this.winner = this.field[index][0]
+        this.isWin = true
+      }
+      if (
+        this.field[0][index] === this.field[1][index] &&
+        this.field[1][index] === this.field[2][index] &&
+        this.field[2][index] != null
+      ) {
+        this.winner = this.field[0][index]
+        this.isWin = true
+      }
+    })
+    if (
+      this.field[0][0] === this.field[1][1] &&
+      this.field[2][2] === this.field[1][1] &&
+      this.field[0][0] != null
+    ) {
+      this.winner = this.field[0][0]
       this.isWin = true
     }
-    console.log(this.currentSymbol)
+    if (
+      this.field[0][2] === this.field[1][1] &&
+      this.field[2][0] === this.field[1][1] &&
+      this.field[1][1] != null
+    ) {
+      this.winner = this.field[1][1]
+      this.isWin = true
+    }
+
+    if (this.winner === null && this.isNoMoreTurns === true) {
+      this.isDrawResult = true
+    }
+
+    // console.log(this.field[0][0], this.field[0][1], this.field[0][2])
+    // console.log(this.field[1][0], this.field[1][1], this.field[1][2])
+    // console.log(this.field[2][0], this.field[2][1], this.field[2][2])
+    // console.log(this.winner)
   }
 
   isFinished() {
@@ -44,14 +88,16 @@ class TicTacToe {
     return this.winner
   }
 
-  noMoreTurns() {
-    return this.isNoMoreTurns
-  }
-
   isDraw() {
     return this.isDrawResult
   }
 
+  noMoreTurns() {
+    return this.isNoMoreTurns
+  }
+  getCurrentPlayerSymbol() {
+    return this.currentSymbol
+  }
   getFieldValue(rowIndex, colIndex) {
     return this.field[rowIndex][colIndex]
   }
