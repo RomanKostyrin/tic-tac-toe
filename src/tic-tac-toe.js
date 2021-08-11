497d7eb4050b29343930f89c84f7f3f567eb245f
roman kostyrin <enemy-iubip@mail.ru>
2021-08-11 09:24:47 +0300

    feat: make nextTurn, getCurrentPlayerSymbol and getFieldValue modules

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,21 +1,61 @@
 class TicTacToe {
-  constructor() {}
-
-  getCurrentPlayerSymbol() {
-    return console.log('x')
+  constructor() {
+    this.field = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]
+    this.currentSymbol = 'x'
+    this.isWin = false
+    this.winner = null
+    this.isDrawResult = false
+    this.isNoMoreTurns = false
   }
 
-  nextTurn(rowIndex, columnIndex) {}
+  getCurrentPlayerSymbol() {
+    return this.currentSymbol
+  }
 
-  isFinished() {}
+  nextTurn(rowIndex, columnIndex) {
+    if (this.field[rowIndex][columnIndex]) {
+      return
+    }
+    this.field[rowIndex][columnIndex] = this.currentSymbol
+    this.currentSymbol = this.currentSymbol === 'x' ? 'o' : 'x'
+    let score = 0
+    this.field.forEach((el) => {
+      el.forEach((el) => {
+        if (el === 'x' || el === 'o') {
+          score += 1
+        }
+      })
+    })
+    if (score === 9) {
+      this.isWin = true
+    }
+    console.log(this.currentSymbol)
+  }
 
-  getWinner() {}
+  isFinished() {
+    return this.isWin
+  }
 
-  noMoreTurns() {}
+  getWinner() {
+    return this.winner
+  }
 
-  isDraw() {}
+  noMoreTurns() {
+    return this.isNoMoreTurns
+  }
 
-  getFieldValue(rowIndex, colIndex) {}
+  isDraw() {
+    return this.isDrawResult
+  }
+
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex]
+  }
 }
 
+let game = new TicTacToe()
 module.exports = TicTacToe
